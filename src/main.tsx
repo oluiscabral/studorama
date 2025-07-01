489a579685c0ba6dd38ebd08361136f476692537
oluiscabral <oluiscabral@hotmail.com>
2025-07-01 01:41:15 -0300

    Use analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -6,6 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <SpeedInsights />
     <App />
   </StrictMode>

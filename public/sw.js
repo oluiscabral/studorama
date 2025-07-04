9299446478d682eacf4f1a285622db1d84ae038c
oluiscabral <oluiscabral@hotmail.com>
2025-07-04 03:11:52 -0300

    chore: bump version to 2.3.4

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Lightweight Service Worker optimized for fast initial load
-const CACHE_NAME = 'studorama-v2.3.3';
+const CACHE_NAME = 'studorama-v2.3.4';
 const ESSENTIAL_FILES = [
   '/',
   '/index.html',
